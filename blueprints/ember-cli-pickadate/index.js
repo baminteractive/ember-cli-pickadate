576c08a1d58453c858be997568c8f83966fc092f
Kyle Werner <kyle@wernull.com>
2015-09-11 14:37:52 -0400

    don't add pickadate through this addon, requires pickadate to be added along side this

--- blueprints/ember-cli-pickadate/index.js
+++ blueprints/ember-cli-pickadate/index.js
@@ -6,6 +6,6 @@ module.exports = {
   },
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('pickadate', 'baminteractive/pickadate.js');
+    //return this.addBowerPackageToProject('pickadate', 'baminteractive/pickadate.js');
   }
 };
